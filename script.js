9efce38cc1a04c3086da1dc5fb03908d4f51b45e
bernhardhuettel-svg <bernhardhuettel@icloud.com>
2026-04-03 13:11:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -10,69 +10,41 @@ const papers = [
 ];
 
 let currentLocation = 1;
-let numOfPapers = 4;
-let maxLocation = numOfPapers + 1;
-
-function openBook() {
-    book.style.transform = "translateX(50%)";
-}
-
-function closeBook(isAtBeginning) {
-    if(isAtBeginning) {
-        book.style.transform = "translateX(0%)";
-    } else {
-        book.style.transform = "translateX(100%)";
-    }
-}
+let maxLocation = 5; // 4 Papiere + 1
 
 function goNextPage() {
     if(currentLocation < maxLocation) {
-        switch(currentLocation) {
-            case 1:
-                openBook();
-                papers[0].classList.add("flipped");
-                papers[0].style.zIndex = 1;
-                break;
-            case 2:
-                papers[1].classList.add("flipped");
-                papers[1].style.zIndex = 2;
-                break;
-            case 3:
-                papers[2].classList.add("flipped");
-                papers[2].style.zIndex = 3;
-                break;
-            case 4:
-                papers[3].classList.add("flipped");
-                papers[3].style.zIndex = 4;
-                closeBook(false);
-                break;
+        if(currentLocation === 1) {
+            book.classList.add("book-open");
         }
+        
+        const paper = papers[currentLocation - 1];
+        paper.classList.add("flipped");
+        paper.style.zIndex = currentLocation; // Stapelt die umgeblätterten Seiten nach unten
+
+        if(currentLocation === 4) {
+            book.style.transform = "translateX(100%)";
+        }
+        
         currentLocation++;
     }
 }
 
 function goPrevPage() {
     if(currentLocation > 1) {
-        switch(currentLocation) {
-            case 2:
-                closeBook(true);
-                papers[0].classList.remove("flipped");
-                papers[0].style.zIndex = 4;
-                break;
-            case 3:
-                papers[1].classList.remove("flipped");
-                papers[1].style.zIndex = 3;
-                break;
-            case 4:
-                papers[2].classList.remove("flipped");
-                papers[2].style.zIndex = 2;
-                break;
-            case 5:
-                openBook();
-                papers[3].classList.remove("flipped");
-                papers[3].style.zIndex = 1;
-                break;
+        if(currentLocation === 2) {
+            book.classList.remove("book-open");
+            book.style.transform = "translateX(0%)";
         }
+        
+        const paper = papers[currentLocation - 2];
+        paper.classList.remove("flipped");
+        paper.style.zIndex = 5 - (currentLocation - 1); // Holt die Seite wieder nach oben
+
+        if(currentLocation === 5) {
+            book.classList.add("book-open");
+        }
+        
         currentLocation--;
     }
 }
