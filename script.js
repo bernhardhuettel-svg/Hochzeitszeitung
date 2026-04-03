f6d3aef54da9f6d48e589abb4c27a6140ff860ad
bernhardhuettel-svg <bernhardhuettel@icloud.com>
2026-04-03 12:04:41 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+let currentPage = 1;
+const pages = document.querySelectorAll('.page');
+
+function nextPage() {
+    if (currentPage <= pages.length) {
+        const page = document.getElementById(`p${currentPage}`);
+        page.classList.add('flipped');
+        page.style.zIndex = currentPage;
+        currentPage++;
+    }
+}
+
+function prevPage() {
+    if (currentPage > 1) {
+        currentPage--;
+        const page = document.getElementById(`p${currentPage}`);
+        page.classList.remove('flipped');
+        page.style.zIndex = pages.length - currentPage + 1;
+    }
+}
